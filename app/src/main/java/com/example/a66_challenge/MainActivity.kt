98bbba82478a7b30417167ad4bc53e36de28ca2e
JuhyeokLee97 <zero5.two4@gmail.com>
2022-02-03 20:56:37 +0900

    #30 Jetpack_Room: Canvas_Bitmap 저장  - Gradle, Jetpack Room Setting

--- app/src/main/java/com/example/a66_challenge/MainActivity.kt
+++ app/src/main/java/com/example/a66_challenge/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.room.Room
 import com.example.a66_challenge.canvas.fragment.*
 import com.example.a66_challenge.databinding.ActivityMainBinding
 
